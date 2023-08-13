4302877c2e49a9d8e255f78d9aeb8d17d2fc9690
doketor <doketor@gmail.com>
2023-08-13 12:47:37 +0300

    creat_keyboard

--- global_project/keyboard.py
+++ global_project/keyboard.py
@@ -1,11 +1,23 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 
 kbi = InlineKeyboardMarkup(row_width=3)
+bi1=InlineKeyboardButton(text='❤️',
+                         callback_data='like')
+bi2=InlineKeyboardButton(text='👎',
+                         callback_data='dislike')
+bi3=InlineKeyboardButton(text='🔄',
+                         callback_data='next')
+kbi.add(bi1,bi2)
 
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
+kb2=ReplyKeyboardMarkup(resize_keyboard=True)
 b1=KeyboardButton(text='/help')
 b2=KeyboardButton(text='/description')
 b3=KeyboardButton(text='send foto')
 
+b4=KeyboardButton(text='random')
+b5=KeyboardButton(text='back')
+
 kb.add(b1).add(b2,b3)
+kb2.add(b4,b5)
--- global_project/main.py
+++ global_project/main.py
@@ -1,8 +1,9 @@
 from aiogram import Bot, types, Dispatcher, executor
+from aiogram.dispatcher.filters import Text
 import random
 
 from config import TOKEN_API
-from keyboard import kb
+from keyboard import kb, kb2, kbi
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
@@ -35,13 +36,32 @@ async def send_echo(message: types.Message):
     await message.delete()
 
 
-@dp.message_handler()
-async def send_echo(message: types.Message):
-    if message.text == 'send foto':
-        await bot.send_photo(chat_id=message.chat.id,
-                             photo=random.choice(list_url),
-                             caption='Случайны котик')
-        await message.delete()
+@dp.message_handler(Text(equals='send foto'))
+async def open_kb_photo(message: types.Message):
+    await message.answer(text='Что бы отправить рандомную фотку нажим "random"',
+                         reply_markup=kb2)
+    await message.delete()
+
+@dp.message_handler(Text(equals='back'))
+async def open_kb(message: types.Message):
+    await message.answer(text='главное меню',
+                         reply_markup=kb)
+    await message.delete()
+
+@dp.message_handler(Text(equals='random'))
+async def send_random_photo(message: types.Message):
+    await bot.send_photo(chat_id=message.chat.id,
+                         photo=random.choice(list_url),
+                        caption='рандомный котик')
+    await message.delete()
+
+
+@dp.callback_query_handler()
+async def vote_callback(callback: types.CallbackQuery):
+    if callback.data == 'like':
+        await callback.answer(text='тебе нравятся котики')
+    elif callback.data == 'dislike':
+        await callback.answer(text='тебе не нравятся котики')
 
 
 
